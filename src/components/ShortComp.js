fe18c8faa34f4f25f08949045418a352df9ac8b1
ranavijaysingh <vijaysinghgr81@gmail.com>
2023-09-13 23:15:19 +0530

    short component created

--- src/components/ShortComp.js
+++ src/components/ShortComp.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ShortComp() {
+  const [question, setQuestion] = useState('')
   return (
     <div>
-      ShortComp
+      <input placeholder='Question' 
+             value={question} 
+             onChange={(e) => setQuestion(e.target.value)}
+             required
+      />
+      <input placeholder='Answer'
+             readOnly
+      />
     </div>
   )
 }
