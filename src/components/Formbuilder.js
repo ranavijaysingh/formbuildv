f96fad52111b38b2b743cd37d09c95c8e2b08393
Akhilesh <aksi9665@gmail.com>
2023-09-17 01:46:20 +0530

    jai siya ram _/\_

--- src/components/Formbuilder.js
+++ src/components/Formbuilder.js
@@ -1,70 +1,93 @@
-import React, { useState } from 'react'
-import ShortComp from './ShortComp';
-import LongComp from './LongComp';
-import MultipleComp from './MultipleComp';
-import CheckboxComp from './CheckboxComp';
-import Dropdown from './DropdownComp';
-import '../styles/FormStyle.css';
+import React, { useState } from "react";
+import ShortComp from "./ShortComp";
+import LongComp from "./LongComp";
+import MultipleComp from "./MultipleComp";
+import CheckboxComp from "./CheckboxComp";
+import Dropdown from "./DropdownComp";
+import "../styles/FormStyle.css";
+
 export default function Formbuilder() {
+  let [comp, setComp] = useState([]);
+  const [formData, setFormData] = useState([]);
+  const [chooseComponent, setChooseComponent] = useState("");
 
-    const [nofield, setNofield] = useState(0);
-    let [comp, setComp] = useState([]); 
-    
-
-    const renderComp = (component, index) => {
-        switch(component.type){
-            case 'Short answer':
-                return <ShortComp/>    
-            case 'Long answer':
-                return <LongComp/>
-            case 'Multiple choice':
-                return <MultipleComp/>
-            case 'Checkbox':
-                return <CheckboxComp/>
-            case 'Dropdown':
-                return <Dropdown/>
-            default:
-                return null   
-        }
-
+  const renderComp = (component, questionName) => {
+    switch (component) {
+      case "Short answer":
+        return (
+          <ShortComp updateState={updateState} questionDetails={questionName} />
+        );
+      case "Long answer":
+        return <LongComp />;
+      case "Multiple choice":
+        return <MultipleComp />;
+      case "Checkbox":
+        return <CheckboxComp />;
+      case "Dropdown":
+        return <Dropdown />;
+      default:
+        return null;
     }
+  };
 
-    const onSelect = (el) => {
-        const selectedType = el.target.value;
-        setComp([...comp, {nofield:nofield, type:selectedType}])
-        setNofield(nofield+1)
-        el.target.value=""
-    }
+  const updateState = (newData) => {
+    setFormData((prevData) => [...prevData, ...newData]);
+    setChooseComponent("");
+    console.log(formData);
+  };
 
-    const removeComp = (event,index) => {
-        event.preventDefault();
-        console.log(index)
-        const updatedComp = [...comp];
-        updatedComp.splice(index,1);
-        setComp(updatedComp);
-        console.log(comp)
-    }
+  const onSelect = (e) => {
+    const selectedType = e.target.value;
+    setChooseComponent(selectedType);
+    e.target.value = "";
+  };
+
+  const removeComp = (event, index) => {
+    event.preventDefault();
+    console.log(index);
+    const updatedComp = [...comp];
+    updatedComp.splice(index, 1);
+    setComp(updatedComp);
+    console.log(comp);
+  };
+
+  const printFormData = () => {
+    console.log("formData", formData);
+  };
 
   return (
-    <div className='container'>
-        <select onChange={onSelect} className='selectBox'>
-            <option></option>
-            <option>Short answer</option>
-            <option>Long answer</option>
-            <option>Checkbox</option>
-            <option>Multiple choice</option>
-            <option>Dropdown</option>
-        </select>
-        <form>
-            {comp.map((component, index) => {
-                // earlier instead of using index, I was using key={component.nofield}
-                 return (<div key={index}>
-                    {renderComp(component)}
-                    <button onClick={(event) =>removeComp(event, index)}>Remove</button>
-                    {/* context se global state manage karlo useReducer -> stateManage */}
-                </div>) 
-            })}
-        </form>
+    <div className="container">
+      <select onChange={onSelect} className="selectBox">
+        <option></option>
+        <option>Short answer</option>
+        <option>Long answer</option>
+        <option>Checkbox</option>
+        <option>Multiple choice</option>
+        <option>Dropdown</option>
+      </select>
+      <div>
+        {chooseComponent && (
+          <>
+            <h2>Configure Form</h2>
+            {renderComp(chooseComponent)}
+          </>
+        )}
+      </div>
+      <form>
+        {formData &&
+          formData.map((component, index) => {
+            return (
+              <div key={index}>
+                {renderComp(component.type, component.questionName)}
+                <button onClick={(event) => removeComp(event, index)}>
+                  Remove
+                </button>
+              </div>
+            );
+          })}
+      </form>
+
+      <button onClick={printFormData}>SHow State</button>
     </div>
-  )
+  );
 }
--- src/components/ShortComp.js
+++ src/components/ShortComp.js
@@ -1,17 +1,34 @@
-import React, { useState } from 'react'
-import '../styles/short.css';
-export default function ShortComp() {
-  const [question, setQuestion] = useState('')
+import React, { useState } from "react";
+import "../styles/short.css";
+
+export default function ShortComp({ updateState, questionDetails }) {
+  const [newQuestion, setNewQuestion] = useState("");
+
+  const onSubmit = () => {
+    updateState([
+      { type: "Short answer", questionName: newQuestion.questionName },
+    ]);
+  };
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setNewQuestion((prevState) => ({
+      ...prevState,
+      questionName: value,
+    }));
+  };
+
   return (
-    <div className='topShort'>
-      <input placeholder='Question' 
-             value={question} 
-             onChange={(e) => setQuestion(e.target.value)}
-             required
+    <div className="topShort">
+      <input
+        placeholder="Question"
+        value={questionDetails}
+        onChange={onInputChange}
+        required
       />
-      <input placeholder='Answer'
-             readOnly
-      />
+      <input placeholder="Answer" readOnly />
+      <button onClick={onSubmit}>Add Component</button>
     </div>
-  )
+  );
 }
