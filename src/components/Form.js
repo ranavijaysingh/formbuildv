6cd562e1bff88765487e8dbab55a369436285652
ranavijaysingh <vijaysinghgr81@gmail.com>
2023-09-23 02:26:26 +0530

    component wise separation and short, long and multiple type working

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,11 @@
+import { Checkbox } from '@mui/material';
 import React, { useEffect, useState } from 'react'
+import DropdownComponent from './DropdownComponent'
+import CheckboxComponent from './CheckboxComponent'
+import ShortComponent from './ShortComponent'
+import LongComponent from './LongComponent'
+import MultipleComponent from './MultipleComponent'
+
 
 export default function Form() {
     const [formId, setFormId] = useState('650cb712b587093104c5504e')
@@ -10,26 +17,6 @@ export default function Form() {
         multipleChoice: ""
     });
 
-
-    const handleInputChange = (event) => {
-        // const { name, value, type, checked } = event.target;
-
-        // if (type === "checkbox") {
-        //   setFormData((prevData) => ({
-        //     ...prevData,
-        //     checkboxes: {
-        //       ...prevData.checkboxes,
-        //       [name]: checked
-        //     }
-        //   }));
-        // } else {
-        //   setFormData((prevData) => ({
-        //     ...prevData,
-        //     [name]: value
-        //   }));
-        // }
-        console.log("yo")
-    }
     
     const getData = () =>{
         fetch(`http://localhost:8000/forms/${formId}`)
@@ -52,7 +39,40 @@ export default function Form() {
         getData();
     },[data])
 
-    
+    const renderComp = (component) =>{
+        switch(component.type) {
+            case 'Short answer':
+                return (
+                    <ShortComponent
+                        component = {component}
+                    />
+                    );
+            case 'Long answer':
+                return (
+                    <LongComponent
+                        component = {component}
+                    />
+                );
+            case 'Multiple choice':
+                return (
+                    <MultipleComponent
+                        component = {component}
+                    />
+                );
+            case 'Checkbox':
+                return (
+                    <CheckboxComponent
+                    />
+                );
+            case 'Dropdown':
+                return (
+                    <DropdownComponent
+                    />
+                );
+            default:
+                return null;
+        }
+    }
     
     return (
         <div>
@@ -63,58 +83,15 @@ export default function Form() {
                 <h1>Data Received:</h1>
                 <pre>{JSON.stringify(data, null, 2)}</pre>
                 <form>
-                    {data.form.map((item) =>{
-                        return (<div key={item._id}>
-                            <label>{item.question}</label>
-                            {item.type=== 'Short answer' && 
-                            (<input type='text' value={formData.textInput} onChange={handleInputChange}/>)}
-                            {item.type=== 'Long answer' && 
-                            (<textarea type='text' value={item.ans} onChange={handleInputChange}/>)}
-                            {item.type==='Checkbox' && (
-                                <div>
-                                    {item.inputValues.map((opt)=>{
-                                        return(<label>
-                                            <input 
-                                                type='checkbox'
-                                                checked='false'
-                                                value='fsd'
-                                                onChange={handleInputChange}
-                                            />
-                                            {opt}
-                                        </label>)
-                                    })}
-                                </div>
-                            )}
-
-                            {item.type==='Dropdown' && (<select value={"sd"} onChange={handleInputChange}>
-                                    <option value="">Select an option</option>
-                                    {item.inputValues.map((opt)=>{
-                                        return(
-                                            <option value={opt}>{opt}</option>
-                                        )
-                                    })}
-                                </select>
-                            )}
-
-                            {item.type==='Multiple choice' && (<div>
-                                    {item.inputValues.map((opt)=>{
-                                        return(<label>
-                                            <input 
-                                                type='radio'
-                                                checked='false'
-                                                value={opt}
-                                                onChange={handleInputChange}
-                                            />
-                                            {opt}
-                                        </label>)
-                                    })}
-                                </div>
-                            )}
-                        </div>)
-                    })}
-                    
+                    {data.form.map((component) =>(
+                        <div key={component._id}>
+                            {renderComp(component)}
+                        </div>
+                    ))}
+                    <button type='Submit'>Submit</button>
                 </form>
             </div>
-        )}
+            )}
         </div>
-)}
+    )
+}
